e5d5b4b6a7eace1df4c62c94ecc6aa70d0c9f10d
johnny <t511069160@qq.com>
2020-09-02 15:06:32 +0800

    '改变容器中的内容'

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-	res.send('hello, world112')
+	res.send('hello, world112456')
 })
 
 app.listen(8080, function () {
