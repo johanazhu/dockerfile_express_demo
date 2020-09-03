932e9675fb80a242fe81499e7638b26c49a44e53
johnny <t511069160@qq.com>
2020-09-03 19:14:30 +0800

    '防止跨站点请求伪造'

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-	res.send('hello, docker_push自动化111112456 7890再见再见')
+	res.send('hello, docker_push自动化111112456 7890再见再见，防止跨站点请求伪造')
 })
 
 app.listen(8080, function () {
